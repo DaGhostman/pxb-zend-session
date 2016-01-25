25a3cf749f459bf4f4c8466c065e62f38d5bf9ef
Dimitar Dimitrov <daghostman.dimitrov@gmial.com>
2016-01-25 19:14:51 +0000

    Removed argument requirement in configuration class

--- src/SessionModuleConfig.php
+++ src/SessionModuleConfig.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ddimitrov
- * Date: 24/01/16
- * Time: 14:21
- */
 
 namespace PXB\Module\Zend\Session;
 
@@ -17,7 +11,6 @@ class SessionModuleConfig
      * Returns a configuration container, that contains configurations
      * applicable to this module (templates, routes, dependencies, etc)
      *
-     * @param ContainerInterface $container
      * @return ConfigProvider
      *
      * @codeCoverageIgnore
